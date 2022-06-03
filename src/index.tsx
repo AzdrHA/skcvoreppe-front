323af709708505f09a2c252d73dc5b3757f4ec45
Azdra <baptiste.brand2002@gmail.com>
2022-06-03 19:18:04 +0200

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {BaseRouter} from '@app/router/BaseRouter';
+import '@styles/app.scss';
 import '@styles/tailwind.css';
+import './i18n';
 
 ReactDOM.render(
     <React.StrictMode>
