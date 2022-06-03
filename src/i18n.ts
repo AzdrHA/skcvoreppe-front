dec5b4033d2ba2904f247a97d4afe8db4fb1d58f
Azdra <baptiste.brand2002@gmail.com>
2022-06-03 19:13:31 +0200

    Install i18n + add config

---
+++ src/i18n.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+i18n
+    .use(initReactI18next)
+    .init({
+      resources: {
+        fr: {
+          translation: require('./translations/fr.json'),
+        },
+      },
+      lng: 'fr',
+      fallbackLng: 'fr',
+      interpolation: {
+        escapeValue: false,
+      },
+    });
