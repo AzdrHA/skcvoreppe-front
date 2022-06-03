f8b6cdeb59eba2e7e6d6467bd7aea87f8d5f2a2c
Azdra <baptiste.brand2002@gmail.com>
2022-06-03 19:15:07 +0200

    Update Not Found page

--- src/screens/Exception/NotFound.tsx
+++ src/screens/Exception/NotFound.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {ButtonLink} from '@components/style/button/ButtonLink';
+import {useTranslation} from 'react-i18next';
 
 export const NotFound = () => {
+  const {t} = useTranslation();
+
   return (
-    <div>
-      404 not found
+    <div className={'w-screen h-screen flex items-center justify-center'}>
+      <div className={'not-found-content text-center'}>
+        <h1 className={'title font-medium text-9xl drop-shadow-xl'}>404</h1>
+        <div className={'subtitle mt-2 mb-3'}>
+          <h2 className={'font-medium'}>{t('PAGE_DOES_NOT_LOAD')}</h2>
+          <p>{t('DONT_WORRY_WE_WILL_SHOW_YOU_ANOTHER_WAY')}</p>
+        </div>
+        <ButtonLink>{t('BACK_TO_HOME_PAGE')}</ButtonLink>
+      </div>
     </div>
   );
 };
